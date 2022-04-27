6d94eb299404bceab437e9990b8e32b2076527d5
DevLouisFR <104511388+DevLouisFR@users.noreply.github.com>
2022-04-27 16:25:01 +0200

    Add files via upload

---
+++ script/playlist.js
@@ -0,0 +1,58 @@
+// AUDIO PLAYLIST
+let audioPlaylist = [
+    {
+        "artist" : "Daniel Pemberton",
+        "title" : "The Devil & The Huntsman",
+        "cover" : "audios/thedevil&thehuntsman.jpg",
+        "file":"audios/thedevil&thehuntsman.mp3"
+    },
+    {
+        "artist" : "Florent Pagny",
+        "title" : "Le soldat",
+        "cover" : "audios/lesoldat.jpg",
+        "file":"audios/lesoldat.mp3"
+    },
+    {
+        "artist" : "Sting",
+        "title" : "Shape Of My Heart",
+        "cover" : "audios/shapeofmyheart.jpg",
+        "file":"audios/shapeofmyheart.mp3"
+    },
+
+
+]
+
+// VIDEO PLAYLIST
+
+let videoPlaylist = [
+    {
+        "title" : "Tu ne tueras point - Bande Annonce",
+        "description" : "",
+        "cover" : "videos/tunetueraspoint.jpg",
+        "file":"videos/tunetueraspoint.mp4",
+        "link" : "https://www.youtube.com/watch?v=h1Jv5WdOrz8&ab_channel=FilmsActu"
+    },
+    {
+        "title" : "Au revoir là-haut - Bande Annonce ",
+        "description" : "",
+        "cover" : "videos/aurevoirlahaut.jpg",
+        "file":"videos/aurevoirlahaut.mp4",
+        "link" : "https://www.youtube.com/watch?v=4Lst66pETxA&ab_channel=Gaumont"
+    },
+    {
+        "title" : "FURY  - Bande Annonce ",
+        "description" : "",
+        "cover" : "videos/fury.jpg",
+        "file":"videos/fury.mp4",
+        "link" : "https://www.youtube.com/watch?v=PJTkKIxNJAg&ab_channel=SonyPicturesFr"
+    },
+    {
+        "title" : "Le Roi arthur - La légende d'Excalibur - Bande Annonce ",
+        "description" : "",
+        "cover" : "videos/leroiarthur.jpg",
+        "file":"videos/leroiarthur.mp4",
+        "link" : "https://www.youtube.com/watch?v=Lg2g4WTkvJs&ab_channel=WarnerBros.France"
+    },
+]
+
+
